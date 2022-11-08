0e671367ec1743a6f743a0502d62499b5f1b8072
Cansu Dinçer <cd7@sanger.ac.uk>
2022-11-08 13:46:22 +0000

    off targets

--- BEstimate/off_targets.py
+++ BEstimate/off_targets.py
@@ -8,13 +8,51 @@
 # -----------------------------------------------------------------------------------------#
 
 # Import necessary packages
-import os, pandas, argparse
-from BEstimate.BEstimate.BEstimate import find_pam_protospacer, add_genomic_location, take_input
+import os, pandas, argparse, re
 from Bio import SeqIO
 
 
 # -----------------------------------------------------------------------------------------#
 # Take inputs
+
+def take_input():
+	parser = argparse.ArgumentParser(prog="BEstimate Off targets",
+									 usage="%(prog)s [inputs]",
+									 description="""
+                                     **********************************
+                                     Find and Analyse Base Editor sites
+                                     		 Off Target Analysis
+                                     **********************************""")
+
+	for group in parser._action_groups:
+		if group.title == "optional arguments":
+			group.title = "Inputs"
+		elif "positional arguments":
+			group.title = "Mandatory Inputs"
+
+	# PAM AND PROTOSPACER INFORMATION
+
+	# The NGG PAM will be used unless otherwise specified.
+	parser.add_argument("-pamseq", dest="PAMSEQ", default="NGG",
+						help="The PAM sequence in which features used "
+							 "for searching activity window and editable nucleotide.")
+	parser.add_argument("-pamwin", dest="PAMWINDOW", default="21-23",
+						help="The index of the PAM sequence when starting "
+							 "from the first index of protospacer as 1.")
+	parser.add_argument("-protolen", dest="PROTOLEN", default="20",
+						help="The total protospacer and PAM length.")
+
+
+	# OUTPUT
+
+	parser.add_argument("-o", dest="OUTPUT_PATH", default=os.getcwd() + "/",
+						help="The path for output. If not specified the current directory will be used!")
+
+	parsed_input = parser.parse_args()
+	input_dict = vars(parsed_input)
+
+	return input_dict
+
 
 args = take_input()
 
@@ -31,6 +69,100 @@ else:
 	input_path = args["INPUT_PATH"] + "/"
 
 # -----------------------------------------------------------------------------------------#
+# Functions
+
+
+def find_pam_protospacer(sequence, pam_sequence, searched_nucleotide,
+						 activity_window, pam_window, protospacer_length):
+	"""
+	Finding all possible PAM and protospacer regions on the sequence of the gene.
+	:param sequence: The sequence of the interested gene.
+	:param pam_sequence: The sequence pattern of the PAM region (NGG/NG etc)
+	:param searched_nucleotide: The interested nucleotide which will be changed with BE
+	:param activity_window: The location of the activity window on the protospacer sequence.
+	:param pam_window: The location of the PAM sequence when 1st index of the protospacer is 1.
+	:param protospacer_length: The length of protospacer.
+	:return crisprs: A list of dictionary having sequences and locations of the gRNA targeted
+	gene parts. The indices are indicated when the first index of the gene is 1.
+	"""
+	# Since python index starts from 0, decrease the start position index given from the user
+	activity_window = [activity_window[0] - 1, activity_window[1]]
+	pam_window = [pam_window[0] - 1, pam_window[1]]
+
+	# Using Regular Expressions, specify PAM pattern
+	pam_pattern = ""
+	for nuc in list(pam_sequence):
+		if nuc != "N":
+			pam_pattern += nuc + "{1}"
+		else:
+			pam_pattern += "[ATCG]{1}"
+
+	# Search protospacer length of nucleatide sequence and add PAM pattern after that
+	pattern = r'[ATCG]{%s}%s' % (protospacer_length, pam_pattern)
+	print("Pattern is created!")
+
+	crisprs = []
+	print("Pattern is searching through the sequence...")
+	for nuc_index in range(0, len(sequence)):
+
+		# One by one in the given sequence
+		if nuc_index + pam_window[1] <= len(sequence):
+
+			# Add started nucleotide index total length of targeted base editing site (PAM index)
+			sub_sequence = sequence[nuc_index:nuc_index + pam_window[1]]
+
+			# Search regex pattern inside the sub sequence
+			for match_sequence in re.finditer(pattern, sub_sequence):
+
+				# If there is match, then check if searched nucleotide inside the activity window
+				if searched_nucleotide in list(match_sequence.group()[activity_window[0]:activity_window[1]]):
+					activity_sequence = match_sequence.group()[activity_window[0]:activity_window[1]]
+
+					# If searched nucleotide is also there, add the sequence inside crisprs!
+					crisprs.append({"index": [nuc_index, nuc_index + pam_window[1]],
+									"crispr": match_sequence.group(),
+									"activity_seq": activity_sequence})
+				else:
+					crisprs.append({"index": [nuc_index, nuc_index + pam_window[1]],
+									"crispr": match_sequence.group(),
+									"activity_seq": "No window"})
+
+	if crisprs is not []: print("CRISPRs are found!")
+	return crisprs
+
+def add_genomic_location(sequence_range, crispr_dict, crispr_direction, strand):
+	"""
+	Adding genomic location info on crisprs found by extract_activity_window() function.
+	:param sequence_range: The range of the sequence on the genome (from Ensembl)
+	:param crispr_dict: The CRISPR dictionary created by extract_activity_window()
+	:param crispr_direction: The direction of the created CRISPR (left or right)
+	:param strand: The strand of the given gene (-1 or 1)
+	:return crispr_seq: The sequence of the CRISPR
+	:return genomic_location: The genomic coordinate of the above CRISPR on genome
+	"""
+	# Prepare the sequence range according to the strand of the gene
+
+	new_range = []
+	if strand == 1:
+		new_range = sequence_range
+	elif strand == -1:
+		new_range = [sequence_range[1], sequence_range[0]]
+
+	# Look for both direction
+	crispr_seq, genomic_location = crispr_dict["crispr"], ""
+
+	if crispr_direction == "right":
+		genomic_start = new_range[0] + crispr_dict["index"][0]
+		genomic_end = (genomic_start + len(crispr_seq)) - 1
+		genomic_location = str(genomic_start) + "-" + str(genomic_end)
+
+	elif crispr_direction == "left":
+		genomic_start = new_range[1] - crispr_dict["index"][0]
+		genomic_end = (genomic_start - len(crispr_seq)) + 1
+		genomic_location = str(genomic_end) + "-" + str(genomic_start)
+
+	return crispr_seq, genomic_location
+
 
 def get_genomic_crisprs(pam_sequence, pam_window, protospacer_length):
 
